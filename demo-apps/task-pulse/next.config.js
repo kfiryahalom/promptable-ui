78ecba3e518d12ed420c4345d3fb24577711464e
kfiryahalom <kfiryahalom@gmail.com>
2026-05-02 23:20:39 +0300

    add authentication flow with login page and session management

--- demo-apps/task-pulse/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['promptable-ui'],
-  experimental: {
-    serverComponentsExternalPackages: ['better-sqlite3'],
-  },
-};
-
-module.exports = nextConfig;
